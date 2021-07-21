53f3deee55690204814debd08490223a6f0d6574
Jesus Gonzalez <wilberthg16@gmail.com>
2021-07-20 20:14:54 -0500

    Organizacion inicial del proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Home from './web/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div>
+				{/* Aqui va el header */}
+
+				<Switch>
+					<Route path="/" exact >
+						<Home />
+					</Route>
+				</Switch>
+
+				{/* Aqui va el Footer */}
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/web/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import '../scss/web/home.scss';
+
+class Home extends Component {
+    render(){
+        return (
+            <div className="home-banner">
+                Banner
+            </div>
+        );
+    }
+}
+
+export default Home;

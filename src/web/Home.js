0d23381cf9a9c089c6e6097269a46d696318d490
Jesus Gonzalez <wilberthg16@gmail.com>
2021-07-20 21:04:08 -0500

    Agrege el video banner background

--- src/web/Home.js
+++ src/web/Home.js
@@ -6,7 +6,9 @@ class Home extends Component {
     render(){
         return (
             <div className="home-banner">
-                Banner
+                <div
+                    className="video-banner-bg"
+                    dangerouslySetInnerHTML={{ __html: '<iframe src="https://player.vimeo.com/video/11550303?autoplay=1&color=ffffff&title=0&byline=0&portrait=0&loop=10&background=1&muted=1" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>'}} />                    
             </div>
         );
     }
